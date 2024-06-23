e79cbf4db105d1ee0ad9c2319a66c7735141d532
Dakshayanigitlearn <konduridakshayani@gmail.com>
2024-06-22 21:17:56 -0400

    temporary changes

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
+ urlpatterns = [
        path('admin/', admin.site.urls),
        path('djangoapp/', include('djangoapp.urls')),
        path('', TemplateView.as_view(template_name="Home.html")),
